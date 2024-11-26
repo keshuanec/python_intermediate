ce3c77841a1fb3053ce2b1058f7b0594a4ebb190
keshuanec <jirkahavlas@gmail.com>
2024-11-26 19:56:38 +0100

    test 9 prace

--- context_example.py
+++ context_example.py
@@ -1,20 +1,22 @@
-from time import time
+from datetime import datetime
 
-class TimeMeasurment:
+
+class TimeMeasurment():
     def __init__(self):
+        pass
 
     def __enter__(self):
-        start = time()
+        self.start = datetime.now()
+        print(f"spoustim vypocet v {self.start.strftime('%H:%M:%S')}")
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        end = time()
-        print(end - start)
+        self.end = datetime.now()
+        print(f"dokonceno v case {self.end.strftime('%H:%M:%S')}. Blok trval {self.end - self.start}")
 
 
-
-with TimeMeasurment() as t: #"Spouštím výpočet v čase 18:59.03"
+with TimeMeasurment() as t:  # "Spouštím výpočet v čase 18:59.03"
     cislo = 1
     for i in range(10000000):
         cislo += i
-    print(cislo) #49999995000001
-#"Dokončeno v čase 18:59.05 - blok trval 3 sekundy
+    print(cislo)  # 49999995000001
+# "Dokončeno v čase 18:59.05 - blok trval 3 sekundy
