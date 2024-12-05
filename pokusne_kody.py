271dd389b3b3f4b0048aadeaa9f300c3207f4091
keshuanec <jirkahavlas@gmail.com>
2024-12-05 19:48:41 +0100

    multithreading

--- pokusne_kody.py
+++ pokusne_kody.py
@@ -1,5 +1,11 @@
 from random import randint
+import task13_multithread
+import timeit
 
 
-while throw ==
-throw = randint(1,6)
+#
+# while throw ==
+# throw = randint(1,6)
+#
+# print(task13_multithread.nasobeni(100))
+# print(task13_multithread.scitani(100))
